e299dc4f5a473313165687a3498dba7b7d7a54cb
Ace <2479909899@qq.com>
2021-01-28 17:17:34 +0800

    add: tree

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,100 +1,28 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-      <el-row>
-        <el-col :span="23">
-          <div class="block">
-            <span class="demonstration">PRIME1</span>
-            <el-slider v-model="PRIME1" @change="showData"></el-slider>
-          </div>
-          <div class="block">
-            <span class="demonstration">PRIME2</span>
-            <el-slider v-model="PRIME2" @change="showData"></el-slider>
-          </div>
-          <div class="block">
-            <span class="demonstration">SLAT</span>
-            <el-slider v-model="SLAT" :max="99999999999" @change="showData"></el-slider>
-          </div>
-
-          <div class="block">
-            <span class="demonstration">CODE_LENGTH</span>
-            <el-slider v-model="CODE_LENGTH" :max="8" show-stops @change="showData"></el-slider>
-          </div>
-        </el-col>
-    </el-row>
-
-    <line-chart :chart-data="lineChartData" :x-axis="CHARS" />
+    <Node></Node>
+<!--    <Vode></Vode>-->
   </div>
 </template>
 
 <script>
-import LineChart from "./LineChart";
 
+// import Vode from "./Vode";
+import Node from "./Node";
 export default {
   name: 'HelloWorld',
-  components: {LineChart},
+  components: {Node},
   props: {
     msg: String
   },
   data() {
-    return {
-      PRIME1:3,
-      PRIME2:11,
-      SLAT: 1234561,
-      CODE_LENGTH: 6,
-      CHARS_LENGTH: 32,
-      CHARS:['F', 'L', 'G', 'W', '5', 'X', 'C', '3',
-        '9', 'Z', 'M', '6', '7', 'Y', 'R', 'T', '2', 'H', 'S', '8', 'D', 'V', 'E', 'J', '4', 'K',
-        'Q', 'P', 'U', 'A', 'N', 'B'],
-      val:[],
-      lineChartData:  []
-    }
+    return {}
   },
   created() {
-    this.showData()
+
   },
-  methods:{
-    gen(id)
-    {
-      //补位
-      id = id * this.PRIME1 + this.SLAT;
-      //将 id 转换成32进制的值
-      let b = Array();
-      //32进制数
-      b[0] = id;
-      for (let i = 0; i < this.CODE_LENGTH - 1; i++) {
-        b[i + 1] = Math.floor(b[i] / this.CHARS_LENGTH);
-        //按位扩散
-        b[i] = (b[i] + i * b[0]) % this.CHARS_LENGTH;
-      }
-      let s = 0;
-      for (let j = 0; j < this.CODE_LENGTH - 1; j++) {
-        s += b[j];
-      }
-      b[this.CODE_LENGTH - 1] = s * this.PRIME1 % this.CHARS_LENGTH;
-
-
-      //进行混淆
-      let str = "";
-      for (let i = 0; i < this.CODE_LENGTH; i++) {
-        //codeIndexArray[i] = b[i * PRIME2 % CODE_LENGTH];
-        let index = b[i * this.PRIME2 % this.CODE_LENGTH];
-        str = str.concat( this.CHARS[index]);
-        this.val[index]++
-      }
-      // console.log(id, str);
-    },
-    showData(){
-      // console.log(this.PRIME1, this.PRIME2, this.SLAT, this.CODE_LENGTH);
-      this.val = [
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-      ];
-      for (let i=0;i<100000;i++) {
-        this.gen(i);
-      }
-      this.lineChartData = this.val;
-    }
-  }
+  methods:{}
 }
 </script>
 
---
+++ src/components/Node.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+<!--        <el-row>-->
+<!--            <el-col :span="23">-->
+<!--                <div class="block">-->
+<!--                    <span class="demonstration">PRIME1</span>-->
+<!--                    <el-slider v-model="PRIME1" @change="showData"></el-slider>-->
+<!--                </div>-->
+<!--            </el-col>-->
+<!--        </el-row>-->
+
+        <tree :chart-data="chart" />
+    </div>
+</template>
+
+<script>
+    import Tree from "./Tree";
+
+    export default {
+        name: "Node",
+        components: {Tree},
+        data() {
+            return {
+                PRIME1:3,
+                PRIME2:11,
+                val:[],
+                chart:  []
+            }
+        },
+        created() {
+            this.showData()
+            console.log(this.chart)
+        },
+        methods:{
+            gen()
+            {
+
+            },
+            showData(){
+                // console.log(this.PRIME1, this.PRIME2, this.SLAT, this.CODE_LENGTH);
+                this.val = [{
+                    'name':'fare',
+                    'children':[
+                        {'name': 'abc', children:[
+                                {'name': 'abc-1'},
+                                {'name': 'abc-2'},
+                                {'name': 'abc-3'},
+                            ]
+                        },
+                        {'name': 'wang', children:[{}]},
+                        {'name': '红', children:[{}]},
+                    ]
+                }]
+
+
+                this.chart = this.val;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
